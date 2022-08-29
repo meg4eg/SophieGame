82d8bbdd2e05350824df5aacc2f40a065358e88e
Зрячев Роман <zryachev@evolenta.ru>
2022-08-29 12:05:58 +0300

    фиксы

--- nuxt1/pages/index.vue
+++ nuxt1/pages/index.vue
@@ -1,9 +1,33 @@
 <template>
-  <Tutorial/>
+  <div>
+    <h1>SophieGame</h1>
+    <AddPlayer></AddPlayer>
+
+    <PlayersList></PlayersList>
+
+    <ul>
+<!--      <li v-for="(question, index) in questions" :key="index">Q: {{ question.attributes.question }}</li>-->
+    </ul>
+  </div>
+
 </template>
 
 <script>
 export default {
-  name: 'IndexPage'
+  name: 'IndexPage',
+  data() {
+    return {
+      questions: []
+    }
+  },
+  created() {
+    this.getQuestion()
+  },
+  methods: {
+    async getQuestion() {
+      const questions = await this.$axios.$get('http://localhost:1337/api/questions');
+      this.questions = questions.data
+    }
+  },
 }
 </script>
