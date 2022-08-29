b42ebed900bc6851fe81d3569363a54fd497cef6
Зрячев Роман <zryachev@evolenta.ru>
2022-08-29 12:05:28 +0300

    Компонент со списком игроков

---
+++ nuxt1/components/PlayersList.vue
@@ -0,0 +1,18 @@
+<template>
+  <ul>
+    <li v-for="(player, index) in getPlayers" :key="index">{{ player }}</li>
+  </ul>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+
+export default {
+  name: "PlayersList",
+  computed: mapGetters(['getPlayers']),
+}
+</script>
+
+<style scoped>
+
+</style>
